4d71f6045f576efc4299c56ea37c4fa4c89b3f64
morteza <ansarinia@me.com>
2022-10-12 12:11:37 +0200

    revert back to KL

--- cogponder/losses/cog_loss.py
+++ cogponder/losses/cog_loss.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 
 
 class CognitiveLoss(nn.Module):
@@ -37,7 +38,8 @@ class CognitiveLoss(nn.Module):
                 loss = (_halt_steps - _rt_true).abs().float().mean()
                 total_loss += loss
         else:
-            total_loss = (rt_pred - rt_true).abs().float().mean()
+            # total_loss = (rt_pred - rt_true).abs().float().mean()
+            total_loss = self.rt_dist_loss(rt_pred, rt_true)
 
         return total_loss
 
@@ -53,18 +55,22 @@ class CognitiveLoss(nn.Module):
             Shape (batch_size, steps) of type int.
         """
 
-        steps = rt_true.max().item()  # maximum number of steps in the batch
+        # steps = rt_true.max().item()  # maximum number of steps in the batch
 
         # 1. compute distributions
-        rt_true_dist = torch.zeros((steps,))
+        rt_true_dist = rt_pred.new_zeros((self.max_steps + 1,), dtype=torch.float)
         rt_true_idx, rt_true_cnt = torch.unique(rt_true, return_counts=True)
         rt_true_dist[rt_true_idx.long()] += rt_true_cnt
 
-        rt_pred_dist = torch.zeros((steps,))
+        rt_pred_dist = rt_pred.new_zeros((self.max_steps + 1,), dtype=torch.float)
         rt_pred_idx, rt_pred_cnt = torch.unique(rt_pred, return_counts=True)
         rt_pred_dist[rt_pred_idx.long()] += rt_pred_cnt
 
-        # 3. compute the KL divergence between the two distributions
+        # 2. normalize
+        # rt_true_dist = F.normalize(rt_true_dist, p=1, dim=0)
+        # rt_pred_dist = F.normalize(rt_pred_dist, p=1, dim=0)
+
+        # 3. compute the KL divergence between the two normalized distributions
         loss = self.kl_div(rt_pred_dist, rt_true_dist)
 
         return loss
