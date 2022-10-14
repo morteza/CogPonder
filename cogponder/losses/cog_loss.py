03e64eaa17025f717eb8479cf64e759485c2e68d
morteza <ansarinia@me.com>
2022-10-14 17:20:57 +0200

    update timing loss

--- cogponder/losses/cog_loss.py
+++ cogponder/losses/cog_loss.py
@@ -59,12 +59,9 @@ class CognitiveLoss(nn.Module):
         steps = rt_true.max().item()  # maximum number of steps in the batch
 
         # 1. compute RT_TRUE distribution
-        rt_true_loc = rt_true.mean()
-        rt_true_scale = rt_true.std()
-        rt_true_lognorm = torch.distributions.LogNormal(rt_true_loc, rt_true_scale)
-        rt_true_dist = rt_pred.new_zeros((self.max_steps + 1,), dtype=torch.float)
-        for step in range(self.max_steps):
-            rt_true_dist[step] = rt_true_lognorm.log_prob(step).exp()
+        rt_true_norm = torch.distributions.Normal(rt_true.mean(), rt_true.std())
+        rt_true_dist = rt_true_norm.log_prob(torch.arange(0, self.max_steps)).exp()
+        print('true mean', rt_true.detach().mean())
 
         # 1. compute RT_PRED distribution
         rt_pred_dist = rt_pred.new_zeros((self.max_steps + 1,), dtype=torch.float)
