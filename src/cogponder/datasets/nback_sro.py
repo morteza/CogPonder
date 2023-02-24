c790691408cbb456ea2866d84a87ac14ec87e310
morteza <ansarinia@me.com>
2023-02-24 11:10:18 +0100

    WIP: multi-subject embeddings and data loaders

--- src/cogponder/datasets/nback_sro.py
+++ src/cogponder/datasets/nback_sro.py
@@ -34,7 +34,7 @@ class NBackSRODataset(Dataset):
 
         # load and cleanup the data
         self.X, self.trial_types, self.is_targets, self.responses, self.response_times = \
-            self.prepare_data(worker_id='s521', n_back=self.n_back)
+            self.prepare_data(self.n_subjects, self.n_back)
 
     def __len__(self):
         """Get the number of samples.
@@ -45,32 +45,35 @@ class NBackSRODataset(Dataset):
         """Get a feature vector and it's target label.
         """
 
-        return (self.X[idx, :],
-                self.trial_types[idx],
-                self.is_targets[idx],
-                self.responses[idx],
-                self.response_times[idx, :])
+        return (
+            self.X[idx, :],
+            self.trial_types[idx],
+            self.is_targets[idx],
+            self.responses[idx],
+            self.response_times[idx])
 
-    def prepare_data(self, worker_id, n_back):
+    def prepare_data(self, n_subjects, n_back, **kwargs):
         """[summary]
         # TODO required data columns:
         #   subject_index, trial_index, stimulus_index, accuracy, response_time
 
         Args:
-            n_subjects (int): [description]
-            n_trials (int): number of trials per subject including warm-up
-            n_stimuli (int): [description]
+            n_subjects (int): number of subjects to load. -1 for all subjects.
+            n_back (int): number of items in the N-back sequence
+            worker_ids (list[str], optional): list of SRO worker ids to load. Defaults to None.
 
         Returns:
             X: stimulus features
-                shape: (n_subjects, n_trials - n_back, n_back)
-            targets: whether a trial was a match or not
+                shape: (n_subjects * n_trials, n_back)
+            trial_types: whether a trial was a match or not
                 To match tensor datatypes, 0 is False, and 1 is True.
-                shape: (n_subjects, n_trials - n_back)
-            responses: target labels, either 0 or 1
-                shape: (n_subjects, n_trials - n_back)
-            response_times: response time of each trial
-                shape: (n_subjects, n_trials - n_back)
+                shape: (n_subjects * n_trials)
+            is_corrects: whether a response was correct or not
+                shape: (n_subjects * n_trials)
+            responses: whether it was responded as a match or not
+                shape: (n_subjects * n_trials)
+            response_times: response time
+                shape: (n_subjects * n_trials)
         """
 
         data = pd.read_csv(
@@ -82,30 +85,36 @@ class NBackSRODataset(Dataset):
                 'stim_word': 'category',
                 'condition': 'category'})
 
-        # filter out practice and no-response trials
-        data = data.query('worker_id==@worker_id and '
+        # figure out worker_ids to load
+        worker_ids = kwargs.get('worker_ids', [])
+
+        if len(worker_ids) == 0:
+            worker_ids = data['worker_id'].unique()[:n_subjects]
+
+        # filter out worker_ids and practice trials
+        data = data.query('worker_id in @worker_ids and '
                           'exp_stage == "adaptive" and '
                           # 'block_num == 0.0 and '
                           'load == @n_back').copy()
 
-        data = data.sort_values(['block_num', 'trial_num'])
+        data = data.sort_values(['worker_id', 'block_num', 'trial_num'])
 
         data['worker_id'] = data['worker_id'].astype('category')
 
         stimuli = data['stim'].str.upper().astype('category').cat.codes.values
 
-        X = torch.tensor(stimuli).reshape(1, -1)
-        X = X.unfold(1, n_back + 1, 1)  # sliding window of size n_back
-        X = torch.cat([torch.zeros(X.shape[0], 2, X.shape[2]), X], dim=1)
+        X = torch.tensor(stimuli)
+        X = X.unfold(0, n_back + 1, 1)  # sliding window of size n_back
+        X = torch.cat([torch.zeros(n_back, X.size(1)), X])  # pad  with zeros (burn-in trials)
 
         # add subject_index as the first column of X
-        X_ids = torch.tensor(data['worker_id'].cat.codes.values).reshape(1, -1)
+        X_ids = torch.tensor(data['worker_id'].cat.codes.values).reshape(-1, 1)  # (n_trials, 1)
 
-        X = torch.cat([X_ids.unsqueeze(-1), X], dim=2)
+        X = torch.cat([X_ids, X], dim=1)
 
-        is_targets = (data['stim'].str.lower() == data['target'].str.lower()).values
+        is_targets = data['stim'].str.lower() == data['target'].str.lower()
 
-        trial_types = torch.tensor(is_targets).reshape(1, -1).int()
+        trial_types = torch.tensor(is_targets.values).reshape(1, -1).int()
 
         # response (either matched or not-matched)
         responses = torch.tensor(data.key_press.astype('category').cat.codes.values)
@@ -125,19 +134,19 @@ class NBackSRODataset(Dataset):
             min_rt = torch.min(valid_rts, dim=1, keepdim=True)[0]
             self.non_decision_time = min_rt - self.response_step_interval
 
-        # subtract non decision time
+        # subtract non-decision time from RT
         response_times = torch.where(response_times <= 0,
                                      0, response_times - self.non_decision_time)
 
-        # convert to steps
+        # convert RTs to steps
         response_steps = torch.round(response_times / self.response_step_interval).int()
 
-        valid_trials_mask = ~data['target'].isna()
+        # discard trials with invalid targets, e.g., burn-in trials
+        valid_trials_mask = data['target'].notna().values
+        X = X[:, valid_trials_mask, :]
+        trial_types = trial_types[:, valid_trials_mask]
+        is_corrects = is_corrects[:, valid_trials_mask]
+        responses = responses[:, valid_trials_mask]
+        response_steps = response_steps[:, valid_trials_mask]
 
-        return (
-            X[:, valid_trials_mask, :],
-            trial_types[:, valid_trials_mask],
-            is_corrects[:, valid_trials_mask],
-            responses[:, valid_trials_mask],
-            response_steps[:, valid_trials_mask]
-        )
+        return (X, trial_types, is_corrects, responses, response_steps)
