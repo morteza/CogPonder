7eaedc518fe8aaff7736f53515f239db509c62c2
morteza <ansarinia@me.com>
2023-04-03 18:01:38 +0200

    fix embedding calculation in the recurrence node

--- src/cogponder/models/recurrence.py
+++ src/cogponder/models/recurrence.py
@@ -11,5 +11,19 @@ class RecurrenceModule(nn.Module):
         self.model = nn.GRUCell(self.inputs_dim, self.embeddings_dim)
 
     def forward(self, x, h):
+        """Forward pass of the recurrence module
+
+        Parameters
+        ----------
+        x : Tensor
+            input data of shape (batch_size, inputs_dim)
+        h : Tensor
+            contextual information of shape (batch_size, embeddings_dim)
+
+        Returns
+        -------
+        Tensor
+            next contextual information of shape (batch_size, embeddings_dim)
+        """
         out = self.model(x, h)
         return out
