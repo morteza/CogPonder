22d0cc011bb6ee489a8ceba0771b1baa85a1911f
Morteza Ansarinia <ansarinia@me.com>
2023-04-11 00:03:13 +0200

    cleanup stroop codes for the final run

--- src/cogponder/data/data_module.py
+++ src/cogponder/data/data_module.py
@@ -1,7 +1,8 @@
 import pytorch_lightning as pl
 import torch
-from torch.utils.data import DataLoader, TensorDataset, random_split, Subset
+from torch.utils.data import DataLoader, TensorDataset, Subset
 from torch.utils.data import Dataset
+import numpy as np
 
 
 class CogPonderDataModule(pl.LightningDataModule):
@@ -17,7 +18,7 @@ class CogPonderDataModule(pl.LightningDataModule):
         super().__init__()
         self.save_hyperparameters(ignore=['dataset'], logger=False)
 
-        self.dataset = TensorDataset(*dataset[:])
+        self.dataset = dataset
 
         self.train_ratio = train_ratio
         self.batch_size = batch_size
@@ -25,9 +26,15 @@ class CogPonderDataModule(pl.LightningDataModule):
         self.num_workers = num_workers
 
     def prepare_data(self):
+
+        # remove invalid trials
+        rts = self.dataset[:][5]
+        self.dataset = TensorDataset(*self.dataset[rts > 0])
+
+        # test/train split
         n_trials = torch.unique(self.dataset[:][0]).shape[0]
         train_idx = torch.where(self.dataset[:][0] <= n_trials * self.train_ratio)[0]
-        test_idx = torch.where(self.dataset[:][0] > n_trials * self.train_ratio)[0]        
+        test_idx = torch.where(self.dataset[:][0] > n_trials * self.train_ratio)[0]
 
         self.train_dataset = Subset(self.dataset, train_idx.tolist())
         self.test_dataset = Subset(self.dataset, test_idx.tolist())
