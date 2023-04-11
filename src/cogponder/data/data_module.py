6b2bd9bff57b41adc8a426f0ec1b048bcad1c22e
Morteza Ansarinia <ansarinia@me.com>
2023-04-11 22:41:11 +0200

    quick fix for outlier RTs in NB

--- src/cogponder/data/data_module.py
+++ src/cogponder/data/data_module.py
@@ -29,7 +29,9 @@ class CogPonderDataModule(pl.LightningDataModule):
 
         # remove invalid trials
         rts = self.dataset[:][5]
-        self.dataset = TensorDataset(*self.dataset[rts > 0])
+        invalid_trials = (rts < 0) | (rts > 200)
+        # invalid_trials = (self.dataset[:][4] < 0)
+        self.dataset = TensorDataset(*self.dataset[~invalid_trials])
 
         # test/train split
         n_trials = torch.unique(self.dataset[:][0]).shape[0]
