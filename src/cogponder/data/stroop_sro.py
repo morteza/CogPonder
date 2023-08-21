7b763b6a7f5cd63b3fcc989f4cb528d00dbd0a68
Morteza Ansarinia <ansarinia@me.com>
2023-08-21 12:10:26 +0200

    WIP: fix plotting

--- src/cogponder/data/stroop_sro.py
+++ src/cogponder/data/stroop_sro.py
@@ -35,17 +35,22 @@ class StroopSRODataset(Dataset):
         'correct_responses': ['correct_response'],
     }
 
+    sro_conditions = {'incongruent': 0, 'congruent': 1}
+    sro_colors = {-1: 'timeout', 66: 'blue', 71: 'green', 82: 'red'}
+
     def __init__(
         self,
-        n_subjects: int = -1,  # -1 means all
+        n_subjects: int = -1,  # -1 means all TODO: use mixture of list[str]/int for reproducibility
         step_duration=10,
         non_decision_time: Union[str, int] = 'auto',
+        shuffle_subjects: bool = True,
         data_path='data/Self_Regulation_Ontology/stroop.csv.gz'
     ):
 
         self.n_subjects = n_subjects
         self.step_duration = step_duration
         self.non_decision_time = non_decision_time
+        self.shuffle_subjects = shuffle_subjects
         self.data_path = data_path
 
         self._data = self.to_tensor_dataset(self.preprocess())
@@ -63,37 +68,54 @@ class StroopSRODataset(Dataset):
 
         return items
 
+    def inverse_transform_context(self, contexts):
+        """Inverse transform context values to original labels.
+        """
+        inv_map = {v: k for k, v in self.sro_conditions.items()}
+        return [inv_map[int(c)] for c in contexts]
+
     def preprocess(self):
 
         data = pd.read_csv(self.data_path, index_col=0)
 
-        worker_ids = data['worker_id'].unique()[:self.n_subjects]  # noqa
+        # shuffle and select a subset of subjects
+        worker_ids = data['worker_id'].unique()
+        if self.shuffle_subjects:
+            np.random.shuffle(worker_ids)
+        worker_ids = worker_ids[:self.n_subjects]
 
-        # select only worker_ids and test trials
+        # select only selected worker_ids and test trials
         data = data.query('worker_id in @worker_ids and exp_stage == "test"').copy()
 
         data.sort_index(inplace=True)
         data['trial_index'] = data.groupby('worker_id').cumcount()
 
-        sro_conditions = {'incongruent': 0, 'congruent': 1}
-        sro_colors = {-1: 'timeout', 66: 'blue', 71: 'green', 82: 'red'}
-
         # map key_press to color names
-        data['key_press'] = data['key_press'].map(sro_colors)
-        data['correct_response'] = data['correct_response'].map(sro_colors)
+        data['key_press'] = data['key_press'].map(self.sro_colors)
+        data['correct_response'] = data['correct_response'].map(self.sro_colors)
 
         # set categories
         data['worker_id'] = data['worker_id'].astype('category')
         data['condition'] = data['condition'].astype('category').cat.set_categories(
-            list(sro_conditions.keys()), ordered=True)
+            list(self.sro_conditions.keys()), ordered=True)
         data['key_press'] = data['key_press'].astype('category').cat.set_categories(
-            list(sro_colors.values()), ordered=True)
+            list(self.sro_colors.values()), ordered=True)
         data['correct_response'] = data['correct_response'].astype('category').cat.set_categories(
-            list(sro_colors.values()), ordered=True)
+            list(self.sro_colors.values()), ordered=True)
         data['stim_color'] = data['stim_color'].astype('category').cat.set_categories(
-            list(sro_colors.values()), ordered=True)
+            list(self.sro_colors.values()), ordered=True)
         data['stim_word'] = data['stim_word'].astype('category').cat.set_categories(
-            list(sro_colors.values()), ordered=True)
+            list(self.sro_colors.values()), ordered=True)
+
+        # compute response steps
+        data['response_step'] = data['rt'] // self.step_duration
+        data['response_step'] = data['response_step'].apply(np.floor).astype('int')
+
+        return data
+
+    def to_tensor_dataset(self, data):
+        """Helper to convert a preprocessed data mapping to a TensorDataset.
+        """
 
         # encode categorical variables
         data['worker_id'] = data['worker_id'].cat.codes.astype('int')   # start at 0
@@ -103,21 +125,11 @@ class StroopSRODataset(Dataset):
         data['stim_color'] = data['stim_color'].cat.codes.astype('float32')
         data['stim_word'] = data['stim_word'].cat.codes.astype('float32')
 
-        # compute response steps
-        data['response_step'] = data['rt'] // self.step_duration
-        data['response_step'] = data['response_step'].apply(np.floor).astype('int')
-
         preprocessed = {k: data[v].values.squeeze() for k, v in self.mappings.items()}
 
         # reshape stimuli to (trials, seq, feature)
         stim = preprocessed['stimuli'].reshape(-1, 1, 2)
         preprocessed['stimuli'] = stim
-
-        return preprocessed
-
-    def to_tensor_dataset(self, preprocessed):
-        """Helper to convert a preprocessed data mapping to a TensorDataset.
-        """
 
         tensors = [torch.Tensor(v) for k, v in preprocessed.items()]
 
